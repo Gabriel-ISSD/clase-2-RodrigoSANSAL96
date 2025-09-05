8d2decd4281cf84c4611ab6c86de4780ac36189c
Rodrigo Sanchez Salguero <rodri-23-11@hotmail.com>
2025-09-05 13:50:24 -0300

    Agrego ejercicio que ingresando un valor me de la multiplicacion de sus primeros 12 valores.

---
+++ src/main/java/for_problemaspropuestos_3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class for_problemaspropuestos_3 {
+    public static void main(String[] args) {
+        int valor,resultado;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Ingrese un valor entre el 1 y el 10: ");
+        valor=sc.nextInt();
+        if (valor >= 1 && valor <= 10) {
+            for(int i=1;i<=12;i++) {
+                resultado = valor*i;
+                System.out.print(resultado + " ");
+            }
+        }else{
+            System.out.println("Valor incorrecto");
+        }
+    }
+}
