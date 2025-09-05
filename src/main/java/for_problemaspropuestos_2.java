461727bbb543720bfa601d555e3eb9cdaf7ece01
Rodrigo Sanchez Salguero <rodri-23-11@hotmail.com>
2025-09-05 13:43:40 -0300

    Agrego ejercicio que muestre al tabla del 3, recorriendo del 3 al 30.

---
+++ src/main/java/for_problemaspropuestos_2.java
@@ -0,0 +1,9 @@
+public class for_problemaspropuestos_2 {
+    public static void main(String[] args) {
+        int i, resultado;
+        for(i=3;i<=30;i++) {
+            resultado = i*3;
+            System.out.print(resultado + " ");
+        }
+    }
+}
