de2f51cb2c6bd5a06d90d8b972cb4c899439fd93
Rodrigo Sanchez Salguero <rodri-23-11@hotmail.com>
2025-09-09 16:55:58 -0300

    Agrego ejercicio table de multiplicacion con parametro

---
+++ src/main/java/TablaMultiplicacion.java
@@ -0,0 +1,22 @@
+public class TablaMultiplicacion {
+    private int tabla;
+
+    public TablaMultiplicacion(int ta) {
+        tabla = ta;
+    }
+
+    public void imprimir(){
+        for (int i=0; i<=10;i++){
+            int resultado=tabla*i;
+            System.out.println(tabla + " * " + i + " = " + resultado);
+        }
+    }
+
+    public static void main(String[] args){
+        TablaMultiplicacion obj=new TablaMultiplicacion(3);
+        obj.imprimir();
+
+        TablaMultiplicacion obj2=new TablaMultiplicacion(5);
+        obj2.imprimir();
+    }
+}
