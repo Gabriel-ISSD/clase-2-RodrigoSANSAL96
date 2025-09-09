9c081a4db12a75927ddbf33fe4116db7967f297f
Rodrigo Sanchez Salguero <rodri-23-11@hotmail.com>
2025-09-09 17:01:47 -0300

    Agrego ejercicio numero aleatorio con parametro y return

---
+++ src/main/java/NumeroAleatorio.java
@@ -0,0 +1,21 @@
+public class NumeroAleatorio {
+    private int minimo,maximo;
+
+    public NumeroAleatorio(int min, int max){
+        minimo=min;
+        maximo=max;
+    }
+
+    public int retornar(){
+        int rango = maximo - minimo + 1;
+        int valor = minimo + (int) (Math.random() * rango);
+        return valor;
+    }
+
+    public static void main(String[] args) {
+        NumeroAleatorio n1 = new NumeroAleatorio(10,20);
+        for(int i=0; i<10;i++){
+            System.out.println(n1.retornar());
+        }
+    }
+}
