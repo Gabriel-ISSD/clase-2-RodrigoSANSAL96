7b3f8f0d8374b6ff3cfc67ef1d417883b0918f73
Rodrigo Sanchez Salguero <rodri-23-11@hotmail.com>
2025-09-09 19:23:36 -0300

    Agrego ejercicio cuadrado con parametro y return

---
+++ src/main/java/Cuadrado2.java
@@ -0,0 +1,21 @@
+public class Cuadrado2 {
+    private int lado;
+
+    public Cuadrado2(int la){
+        lado = la;
+    }
+
+    public String retornar(){
+        int superficie = lado*lado;
+        if(superficie>100){
+            return "El cuadrado es grande";
+        }else{
+            return "El cuadrado es chico";
+        }
+    }
+
+    public static void main(String[] args){
+        Cuadrado2 ladoIngresado = new Cuadrado2(10);
+        System.out.println(ladoIngresado.retornar());
+    }
+}
