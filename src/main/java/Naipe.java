8f64679ec59760b5615e3f1f7d336ce1f75e9d72
Rodrigo Sanchez Salguero <rodri-23-11@hotmail.com>
2025-09-11 17:48:12 -0300

    Agrego ejercicio Naipe con parametro y return

---
+++ src/main/java/Naipe.java
@@ -0,0 +1,25 @@
+public class Naipe {
+    private String tipo;
+    private int numero;
+
+    private Naipe(String t, int num){
+        tipo = t;
+        numero = num;
+    }
+
+    public void imprimir(){
+        System.out.println("El tipo de carta es: "+tipo);
+        System.out.println("El numero de la carta es: "+numero);
+    }
+
+    public static void main(String[] args){
+        Naipe carta1 = new  Naipe("Copa",10);
+        Naipe carta2 = new  Naipe("Oro",8);
+        Naipe carta3 = new Naipe("Espada", 1);
+
+        carta1.imprimir();
+        carta2.imprimir();
+        carta3.imprimir();
+    }
+}
+
