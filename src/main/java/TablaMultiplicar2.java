d3860c0a18b6f66b3e035a650f0c3d2fc0ccf267
Rodrigo Sanchez Salguero <rodri-23-11@hotmail.com>
2025-09-12 16:08:14 -0300

    Agrego problema propuesto tabla multiplicar con metodo con parametro y otro sin

---
+++ src/main/java/TablaMultiplicar2.java
@@ -0,0 +1,26 @@
+public class TablaMultiplicar2 {
+    private int valor;
+
+    public TablaMultiplicar2(int va) {
+        valor = va;
+    }
+
+    public void imprimir(){
+        for (int i = 1; i <= 10; i++){
+            int resultado = valor * i;
+            System.out.println(valor+ " * " + i + " : "+resultado);
+        }
+    }
+    public void imprimir(int cant){
+        for (int i = 1; i <= cant; i++){
+            int resultado = valor * i;
+            System.out.println(valor+ " * " + i + " : "+resultado);
+        }
+    }
+
+    public static void main(String[] args) {
+        TablaMultiplicar2 tabla = new TablaMultiplicar2(3);
+        tabla.imprimir();
+        tabla.imprimir(4);
+    }
+}
