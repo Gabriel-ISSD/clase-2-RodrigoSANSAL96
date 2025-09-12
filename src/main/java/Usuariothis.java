fd364cc2ed7beed010cea8605769cbb13fa142da
Rodrigo Sanchez Salguero <rodri-23-11@hotmail.com>
2025-09-12 14:43:57 -0300

    Agrego ejercicio Usuario con parametro usando this

---
+++ src/main/java/Usuariothis.java
@@ -0,0 +1,22 @@
+public class Usuariothis {
+    private String nombre;
+    private String clave;
+
+    public Usuariothis(String nombre, String clave){
+        this.nombre = nombre;
+        this.clave = clave;
+    }
+
+    public void imprimir(){
+        System.out.println(nombre);
+        System.out.println(clave);
+    }
+
+    public static void main(String[] args){
+        Usuariothis usuario1= new Usuariothis("Rodrigo", "1234");
+        Usuariothis usuario2= new Usuariothis("German", "acasd");
+        usuario1.imprimir();
+        usuario2.imprimir();
+
+    }
+}
