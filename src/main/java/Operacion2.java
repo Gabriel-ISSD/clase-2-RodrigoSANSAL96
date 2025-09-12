72a63de2bd6ce359115e72124a74015d7a7c6e01
Rodrigo Sanchez Salguero <rodri-23-11@hotmail.com>
2025-09-12 16:45:00 -0300

    Agrego problema metodos estaticos

---
+++ src/main/java/Operacion2.java
@@ -0,0 +1,13 @@
+public class Operacion2 {
+
+    public static int sumar(int a, int b) {
+        int s = a + b;
+        return s;
+    }
+
+    public static int restar(int a, int b) {
+        int r  = a - b;
+        return r;
+    }
+
+}
---
+++ src/main/java/PruebaOperacion2.java
@@ -0,0 +1,7 @@
+public class PruebaOperacion2 {
+
+    public static void main(String[] args) {
+        System.out.println("Suma : "+ Operacion2.sumar(5,5));
+        System.out.println("Resta : "+ Operacion2.restar(5,5));
+    }
+}
